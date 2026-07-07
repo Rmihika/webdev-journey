0790592dccd24fc443231dac1ebff873c8853608
Mihika <mihikaraut29@gmail.com>
2026-07-07 21:09:15 +0530

    js basics

---
+++ JAVASCRIPT/Lecture01/app.js
@@ -0,0 +1,14 @@
+console.log("hello mihika");
+
+let a = 10;
+let b = 5;
+
+let output=(`your total bill is: ${a+b} rupees`);
+console.log(output);
+
+let age = 18;
+if(age>=18){
+    console.log(`u are an adult as u are ${age}`);
+} else{
+    console.log(`u are a minor`);
+}
