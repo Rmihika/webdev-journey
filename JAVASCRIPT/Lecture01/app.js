d263534f2fd5304db8c38ebf25a4f4f520717f1f
Mihika <mihikaraut29@gmail.com>
2026-07-10 20:01:01 +0530

    comparison

--- JAVASCRIPT/Lecture01/app.js
+++ JAVASCRIPT/Lecture01/app.js
@@ -6,9 +6,27 @@ let b = 5;
 let output=(`your total bill is: ${a+b} rupees`);
 console.log(output);
 
+
+//comparison operators
 let age = 18;
 if(age>=18){
     console.log(`u are an adult as u are ${age}`);
 } else{
     console.log(`u are a minor as u are under 18`);
+}
+
+console.log("before my if statement");
+let agee=19;
+if(age>=18){
+    console.log("u can vote");
+    console.log(5*age);
+}
+console.log("after my if statement");
+
+let name="mihika";
+if(name=="mihika"){
+    console.log(`welcome ${name}`);
+}
+else{
+    console.log('name not same');
 }
---
+++ JAVASCRIPT/Lecture01/prc2.js
@@ -0,0 +1,13 @@
+let popcornsize='xl';
+if(popcornsize=='xl'){
+    console.log("price is: Rs.250");
+}
+else if(popcornsize==l){
+    console.log("price is: Rs.200");
+}
+else if(popcornsize==m){
+    console.log("price is: Rs.100");
+}
+else if(popcornsize==s){
+    console.log("price is: Rs.50");
+}
