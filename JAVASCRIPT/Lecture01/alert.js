2f0ac4076c1cec15dd3a43d370a099bba2087da0
Mihika <mihikaraut29@gmail.com>
2026-07-12 22:37:20 +0530

    loops

---
+++ JAVASCRIPT/Lecture01/alert.js
@@ -0,0 +1,20 @@
+// alert("hello mihika");
+// console.error("sss");
+
+// prompt("enter ur name:");
+
+// let firstname = prompt("enter your firstname:");
+// let lastname= prompt("enter your lastname:");
+
+// console.log(firstname + " " + lastname) ;
+
+// let num=50;
+// if(num%10==0){
+//     console.log('good')
+// }else{
+//         console.log('bad');
+//     }
+
+let name= prompt("enter ur name:");
+let age= prompt("enter ur age:");
+alert($(name) ,"is", $(age), "years old");
---
+++ JAVASCRIPT/Lecture01/logical.js
@@ -0,0 +1,21 @@
+// let marks =70;
+// if(marks>=33 && marks<=80){
+//     console.log("A");
+// }
+// else{
+//     console.log('fail');
+// }
+
+let marks= 75;
+if(marks>=50 || marks<=80){
+    console.log('pass');
+}
+
+//string should start with a and shld have more than 3 chars
+
+let str="apple";
+if(str[0]=="a" && str.length>3){
+    console.log('good string')
+}else{
+    console.log('bad str');
+}
