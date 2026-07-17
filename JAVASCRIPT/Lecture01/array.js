6687eb0b974d2b0aff7c205eb914d093e391aa35
Mihika <mihikaraut29@gmail.com>
2026-07-17 23:13:50 +0530

    arrays

--- JAVASCRIPT/Lecture01/array.js
+++ JAVASCRIPT/Lecture01/array.js
@@ -7,3 +7,4 @@ let marks=[33, 45, 67, 89, 97];
 let info=["mihika", 19, 89.09]; //mixed array
 
 console.log(`at 1st position we have: ${info[1]}`)
+console.log(`at 1st position we have: ${info[7]}`)
